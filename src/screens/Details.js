f898b0bff2a4fb8e19efdd8b49882cf0b514a8fd
Mounir <takimunir@gmail.com>
2021-12-21 13:53:33 +0100

    add screens and button

---
+++ src/screens/Details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Details = () => {
+    return (
+        <View style={{flex: 1, backgroundColor: "#7F3589"}}>
+            <Text style={{ flexDirection : "row",color: "white"}}>Detail screen</Text>
+        </View>
+    );
+}
+
+export default Details;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, Button} from 'react-native';
+
+const HomeScreen = ({navigation}) => {
+    return (
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' , backgroundColor: "#7F3589"}}>
+            <Button
+                title={"Details"}
+                onPress={()=> navigation.navigate('Details')}
+            />
+        </View>
+    );
+}
+
+export default HomeScreen;
